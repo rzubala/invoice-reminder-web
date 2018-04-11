3d043964f1643e4fea137054ebe2b1009e0a7fae
Rafał Zubala <rzubala@operatus.org>
2018-04-11 09:03:37 +0200

    Initial commit

---
+++ src/main/java/com/zubala/rafal/invoicereminder/InvoiceReminderApplication.java
@@ -0,0 +1,12 @@
+package com.zubala.rafal.invoicereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceReminderApplication.class, args);
+	}
+}
