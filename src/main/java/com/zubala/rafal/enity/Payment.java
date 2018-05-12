e220fbdf57bca0b2180009dc4929289e8718f780
Rafał Zubala <rzubala@operatus.org>
2018-05-12 20:02:31 +0200

    payment entity

---
+++ src/main/java/com/zubala/rafal/enity/Payment.java
@@ -0,0 +1,77 @@
+package com.zubala.rafal.enity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="payment")
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="date")
+	private Date date;
+
+	public Payment() {		
+	}	
+	
+	public Payment(int id, String name, String description, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", name=" + name + ", description=" + description + ", date=" + date + "]";
+	}
+}
