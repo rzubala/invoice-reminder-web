07d155ab7b0f43619cebf4da07d91133e18c1dbe
Rafał Zubala <rzubala@operatus.org>
2018-05-14 20:39:21 +0200

    spring security clean up

--- src/main/java/com/zubala/rafal/config/PaymentReminderAppConfig.java
+++ src/main/java/com/zubala/rafal/config/PaymentReminderAppConfig.java
@@ -28,7 +28,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("com.zubala.rafal")
-@PropertySource({ "classpath:persistence-mysql.properties", "classpath:security-persistence-mysql.properties" })
+@PropertySource({ "classpath:persistence-mysql.properties"})
 public class PaymentReminderAppConfig implements WebMvcConfigurer {
 
 	@Autowired
@@ -80,54 +80,6 @@ public class PaymentReminderAppConfig implements WebMvcConfigurer {
 		return myDataSource;
 	}
 
-	// define a bean for our security datasource
-	
-	@Bean
-	public DataSource securityDataSource() {
-		
-		// create connection pool
-		ComboPooledDataSource securityDataSource
-									= new ComboPooledDataSource();
-				
-		// set the jdbc driver class
-		
-		try {
-			securityDataSource.setDriverClass(env.getProperty("security.jdbc.driver"));
-		} catch (PropertyVetoException exc) {
-			throw new RuntimeException(exc);
-		}
-		
-		// log the connection props
-		// for sanity's sake, log this info
-		// just to make sure we are REALLY reading data from properties file
-		
-		logger.info(">>> security.jdbc.url=" + env.getProperty("security.jdbc.url"));
-		logger.info(">>> security.jdbc.user=" + env.getProperty("security.jdbc.user"));
-		
-		
-		// set database connection props
-		
-		securityDataSource.setJdbcUrl(env.getProperty("security.jdbc.url"));
-		securityDataSource.setUser(env.getProperty("security.jdbc.user"));
-		securityDataSource.setPassword(env.getProperty("security.jdbc.password"));
-		
-		// set connection pool props
-		
-		securityDataSource.setInitialPoolSize(
-				getIntProperty("security.connection.pool.initialPoolSize"));
-
-		securityDataSource.setMinPoolSize(
-				getIntProperty("security.connection.pool.minPoolSize"));
-
-		securityDataSource.setMaxPoolSize(
-				getIntProperty("security.connection.pool.maxPoolSize"));
-
-		securityDataSource.setMaxIdleTime(
-				getIntProperty("security.connection.pool.maxIdleTime"));
-		
-		return securityDataSource;
-	}
-	
 	private Properties getHibernateProperties() {
 
 		// set hibernate properties
--- src/main/java/com/zubala/rafal/config/PaymentReminderSecurityConfig.java
+++ src/main/java/com/zubala/rafal/config/PaymentReminderSecurityConfig.java
@@ -1,31 +1,22 @@
 package com.zubala.rafal.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class PaymentReminderSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private DataSource securityDataSource;
-	
     @Autowired
     private UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.jdbcAuthentication().dataSource(securityDataSource);
         auth.userDetailsService(userDetailsService);
 	}
 
@@ -44,16 +35,4 @@ public class PaymentReminderSecurityConfig extends WebSecurityConfigurerAdapter 
 			.and()
 			.exceptionHandling().accessDeniedPage("/access-denied");
 	}
-
-	/*
-	@Bean
-	public UserDetailsManager userDetailsManager() {
-		
-		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-		
-		jdbcUserDetailsManager.setDataSource(securityDataSource);
-		
-		return jdbcUserDetailsManager; 
-	}
-	*/
 }
